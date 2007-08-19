94bb5210e0d241f7bcb5f2da813759780e7df1e0
Mario.Heiderich <Mario.Heiderich@cee7836c-4537-0410-aa96-9bdc17842071>
2007-08-19 20:50:45 +0000

    completed the fuzzing logic, added more fuzzer options (* and ?), modified the fuzzer.php to create real output, modified the storage and fuzzer class

--- trunk/lib/Fuzzer.php
+++ trunk/lib/Fuzzer.php
@@ -1,44 +1,161 @@
 <?php
 
+/**
+ * Enter description here...
+ *
+ */
 class Fuzzer implements FuzzerInterface  {
 
+	/**
+	 * Enter description here...
+	 *
+	 * @var unknown_type
+	 */
+	protected $elements = array();
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @var unknown_type
+	 */
+	protected $routes = array();
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @var unknown_type
+	 */
+	protected $result = array();
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @param Storage $storage
+	 * @param Route $routes
+	 * @return unknown
+	 */
 	public function __construct(Storage $storage, Route $routes) {
 		
-		var_dump($storage);
-		var_dump($routes);
+		$this->elements = $storage->getElements();
+		$this->routes = $routes->getRoutes();
 		
+		return $this->startFuzzing();
 	}
 
+	/**
+	 * Enter description here...
+	 *
+	 * @return unknown
+	 */
 	protected function startFuzzing() {
-	
+
+		foreach($this->routes as $route) {
+			
+			foreach($route as $item) {
+				
+				$result .= $this->concatenateResult($item);	
+			}
+			$this->result[] = $result;
+			$result = null;
+		}
+		
+		return $this->result;
 	}
 	
-	protected function fuzzLinebreaks() {
+	/**
+	 * Enter description here...
+	 *
+	 * @param unknown_type $item
+	 * @return unknown
+	 */
+	protected function concatenateResult($item) {
+
+		if($item == '?'){
+			return $this->getShuffled($this->elements, false);		
+		} elseif($item == '*'){
+            return $this->getShuffled($this->elements, true);     
+        } elseif($item == 'P'){
+			return $this->getShuffled($this->elements->properties, false);	
+		} elseif($item == 'M') {
+			return $this->getShuffled($this->elements->methods, false);
+		} elseif($item == 'O') {
+			return $this->getShuffled($this->elements->operators, false);
+		} elseif($item == 'B') {
+			return $this->getShuffled($this->elements->parenthesis, false);
+		} elseif($item == 'W') {
+			return $this->getShuffled($this->elements->whitespace, false);
+		} elseif($item == 'D') {
+			return $this->getShuffled($this->elements->delimiters, false);
+		} else {
+			throw new Exception('Invalid route fragment');
+		}
+	}
 	
+	/**
+	 * Enter description here...
+	 *
+	 * @param unknown_type $elements
+	 * @return unknown
+	 */
+	protected function getShuffled($elements, $random = false) {
+		
+		# create controlled string - option: BDMOPW
+		if(is_array($elements)) {
+
+			shuffle($elements);
+			return $elements[0];
+		} else {
+
+			$pool = array();
+
+			# create totally chaotic string - option: *
+			if($random) {
+				foreach($elements as $element) {
+				    $pool = array_merge($element, $pool);
+				}
+				$random = null;
+				$counter = rand(0,500);
+				while($counter != 0) {
+				    shuffle($pool);
+				    $random .= $pool[0];
+				    $counter--;    
+			    }
+			    return $random;
+			} 
+			# create contolled chaotic string - option: ?
+			else {
+				foreach($elements as $element) {
+	                shuffle($element);    
+				    $pool[] = $element[0];
+			    }
+    		}
+
+			shuffle($pool);
+			return $pool[0];
+		}
 	}
 
-	protected function fuzzDelimiter() {
-	
-	}	
-	
-	protected function fuzzParameters() {
-	
+	/**
+	 * Enter description here...
+	 *
+	 * @return unknown
+	 */
+	public function getResult() {
+		
+		return $this->result;
 	}
 	
-	protected function fuzzMethods() {
-	
-	}	
-	
-	protected function fuzzParenthesis() {
-	
-	}	
-	
+	/**
+	 * Enter description here...
+	 *
+	 */
 	public function __destruct() {
 	
 	}
 }
 
 /**
+ * Enter description here...
  *
  */
 interface FuzzerInterface {
--- trunk/lib/abstract/ApiAbstract.php
+++ trunk/lib/abstract/ApiAbstract.php
@@ -5,12 +5,25 @@
  */
 abstract class ApiAbstract {
 	
+	/**
+	 * Enter description here...
+	 *
+	 * @var unknown_type
+	 */
 	protected $results;
 	
+	/**
+	 * Enter description here...
+	 *
+	 */
 	abstract protected function initFuzzing();
 	
+	/**
+	 * Enter description here...
+	 *
+	 * @return unknown
+	 */
 	public function getResults() {
 		return $this->results;
 	}
 }
-?>
