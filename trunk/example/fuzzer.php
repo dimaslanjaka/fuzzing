6dd845051af5aa37430f8f2e22712dacb031294e
Mario.Heiderich <Mario.Heiderich@cee7836c-4537-0410-aa96-9bdc17842071>
2007-08-21 21:07:05 +0000

    did some alignment modifications on the fuzzer.php

--- trunk/example/fuzzer.php
+++ trunk/example/fuzzer.php
@@ -3,30 +3,30 @@
 # fetch the API and the plugin(s)
 require_once('../lib/Api.php');
 require_once('../lib/plugins/Launchpad/Launchpad.php');
-
 # Some HTML Fuzzing
-
 # define the routes
 # currently allowed are W,D,P,M,B,O,V
-$routes = array('MBPVBWD', 'POMVBPOBWD', 'BMVWODBM?P?VO', '??????????', '*');
-
+$routes = array('MBPVBWD', 
+                'POMVBPOBWD', 
+                'BMVWODBM?P?VO', 
+                '??????????', 
+                '*');
 # start {pha:zing}
 $fuzzer = new Api($routes, '../elements/html.json');
-
 # take a look at the results
 echo Launchpad::htmlWrapper($fuzzer);
 
 
 
 # Some Javascript Fuzzing
-
 # define the routes
 # currently allowed are W,D,P,M,B,O,V
-$routes = array('MB*VBWD', 'POM????WD', 'BMVWODBVO', '*');
-
+$routes = array('MB**VBWD', 
+                'POM????WD', 
+                'BMVWODBVO', 
+                '*');
 # start {pha:zing}
 $fuzzer = new Api($routes, '../elements/javascript.json');
-
 # take a look at the results
 echo Launchpad::scriptWrapper($fuzzer);  
 # we can use the Launchpad plugin soon to pass the output to spidermonkey
