7856b8550776e3c3648eb9a986b50997090efc8d
Mario.Heiderich <Mario.Heiderich@cee7836c-4537-0410-aa96-9bdc17842071>
2007-08-21 20:16:53 +0000

    re-added Launchpad plugin stub

---
+++ lib/plugins/Launchpad/Launchpad.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ */
+class Launchpad {
+	
+    public static function pdoExecute(Api $api, $connect = null) {
+    		
+    }
+	
+    public static function shellExecute(Api $api, $connect = null) {
+    	
+    }
+    
+    public static function spidermonkeyExecute(Api $api, $connect = null) {
+    	
+    }
+    
+    public static function htmlWrapper(Api $api, $connect = null) {
+    	
+    }
+    
+}
